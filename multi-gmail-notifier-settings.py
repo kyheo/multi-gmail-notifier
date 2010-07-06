6f245939c6afcc494ad4dc24eeab172f9099b94e
Martin Marrese <marrese@gmail.com>
2010-07-06 18:15:32 -0300

    Settings class refactor.
    - Init receives the app name and pass.
    - Signals are connected directly to the class.
    - Builder is now an attribute of the class.
    - Removed all gtk obejcts from attribute list.
    - Added a method that sets the initial state of the objects.
    - Added a method to set all text entry object texts (set_entry_text).
    - Added a method to set all text entry editable bool (set_entry_editable).
    - Added a method to set all buttons visibility bool (set_buttons_visibility).

--- multi-gmail-notifier-settings.py
+++ multi-gmail-notifier-settings.py
@@ -7,159 +7,117 @@ import gtk
 
 class Settings(object):
     
-    def __init__(self):
-        self.config = gk.GK(APP_NAME, APP_PASS);
+    def __init__(self, app_name, app_pass):
+        self.config = gk.GK(app_name, app_pass);
         self.config.create_app()
         self.config.unlock_app()
 
-        builder = gtk.Builder()
-        builder.add_from_file("multi-gmail-notifier-settings.glade")
-        builder.connect_signals({
-            # Windows
-            'on_settings_window_destroy': self.exit,
-            'on_mf_ok_clicked'          : self.mf_exit,
-
-            # Users List
-            'on_users_list_cursor_changed': self.users_list_changed,
-
-            # Buttons
-            'on_new_clicked'   : self.new_click,
-            'on_save_clicked'  : self.save_click,
-            'on_remove_clicked': self.remove_click,
-            'on_quit_clicked'  : self.exit,
-            'on_cancel_clicked': self.cancel_click
-            })
-
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("multi-gmail-notifier-settings.glade")
+        self.builder.connect_signals(self);
  
-        self.window = builder.get_object("settings_window")
-        
-        self.username_entry = builder.get_object('username_entry')
-        self.password_entry = builder.get_object('password_entry')
-        self.uri_entry      = builder.get_object('uri_entry')
-
-        self.new_btn    = builder.get_object('new')
-        self.save_btn   = builder.get_object('save')
-        self.remove_btn = builder.get_object('remove')
-        self.quit_btn   = builder.get_object('quit')
-        self.cancel_btn = builder.get_object('cancel')
-
-        self.mf_window = builder.get_object('mf_window')
-
-        self.mf_label = builder.get_object('mf_label')
-
-        self.users_list = builder.get_object('users_list')
-
+        self.set_initial_state()
         self.users_list_init()
 
     def show(self):
-        self.window.show()
+        self.builder.get_object("settings_window").show()
         gtk.main()
+
+    def set_initial_state(self):
+        self.set_entry_text('', '', '')
+        self.set_entry_editable(False, False, False)
+        self.set_buttons_visibility(True, False, False, True, False)
 
     def users_list_init(self):
         uts = gtk.TreeStore(str, str, str)
         for u in self.config.get_users():
-            uts.append(None, [
-                        u['item'].get_display_name(),
-                        u['item'].get_secret(),
-                        u['attr']['uri']
-                      ])
+            uts.append(None, [u['item'].get_display_name(), \
+                              u['item'].get_secret(), \
+                              u['attr']['uri']])
 
         col = gtk.TreeViewColumn('Users')
         cell = gtk.CellRendererText()
         col.pack_start(cell, True)
         col.add_attribute(cell, 'text', 0)
         
-        self.users_list.append_column(col)
-        self.users_list.set_model(uts)
+        self.builder.get_object('users_list').append_column(col)
+        self.builder.get_object('users_list').set_model(uts)
 
-    def mf_exit(self, widget):
-        self.mf_window.hide()
-
-    def exit(self, widget):
+    def exit(self):
         self.config.lock_app()
         gtk.main_quit()
 
-    def users_list_changed(self, widget):
+    def set_entry_text(self, username, password, uri):
+        self.builder.get_object('username_entry').set_text(username) 
+        self.builder.get_object('password_entry').set_text(password) 
+        self.builder.get_object('uri_entry').set_text(uri)      
+
+    def set_entry_editable(self, username, password, uri):
+        self.builder.get_object('username_entry').set_editable(username)
+        self.builder.get_object('password_entry').set_editable(password)
+        self.builder.get_object('uri_entry').set_editable(uri)
+
+    def set_buttons_visibility(self, new, save, remove, quit, cancel):
+        self.builder.get_object('new').set_visible(new)
+        self.builder.get_object('save').set_visible(save)
+        self.builder.get_object('remove').set_visible(remove)
+        self.builder.get_object('quit').set_visible(quit)
+        self.builder.get_object('cancel').set_visible(cancel)
+
+    def on_settings_window_destroy(self, widget, data=None):
+        self.exit()
+
+    def on_mf_ok_clicked(self, widget, data=None):
+        self.builder.get_object('mf_window').hide()
+     
+    def on_users_list_cursor_changed(self, widget, data=None):
         model, row = widget.get_selection().get_selected()
         if row is not None:
-            self.username_entry.set_text(model.get_value(row, 0))
-            self.password_entry.set_text(model.get_value(row, 1))
-            self.uri_entry     .set_text(model.get_value(row, 2))
+            self.set_entry_text(model.get_value(row, 0), \
+                                model.get_value(row, 1), \
+                                model.get_value(row, 2))
+            self.set_entry_editable(True, True, True)
+            self.set_buttons_visibility(False, True, True, False, True)
 
-            self.username_entry.set_editable(True)
-            self.password_entry.set_editable(True)
-            self.uri_entry     .set_editable(True)
+    def on_new_clicked(self, widget, data=None):
+        self.set_entry_text('', '', '')
+        self.set_entry_editable(True, True, True)
+        self.set_buttons_visibility(False, True, False, False, True)
 
-            self.new_btn   .set_visible(False)
-            self.save_btn  .set_visible(True)
-            self.remove_btn.set_visible(True)
-            self.quit_btn  .set_visible(False)
-            self.cancel_btn.set_visible(True)
-        
-    def new_click(self, widget):
-        self.username_entry.set_text('')
-        self.password_entry.set_text('')
-        self.uri_entry     .set_text('')
+    def on_save_clicked(self, widget, data=None):
+        ue = self.builder.get_object('username_entry')
+        pe = self.builder.get_object('password_entry')
+        ri = self.builder.get_object('uri_entry')     
+        if not ue.get_text() or not pe.get_text() or not ri.get_text():
+            self.builder.get_object('mf_window').show()
+        else:
+            model, row = self.builder.get_object('users_list').get_selection().get_selected()
+            if row is not None:
+                model.set_value(row, 0, ue.get_text())
+                model.set_value(row, 1, pe.get_text())
+                model.set_value(row, 2, ri.get_text())
+            else:
+                model.append(None, [ue.get_text(), pe.get_text(), ri.get_text()])
 
-        self.username_entry.set_editable(True)
-        self.password_entry.set_editable(True)
-        self.uri_entry     .set_editable(True)
+            self.config.add_user(ue.get_text(), pe.get_text(), ri.get_text())
+            self.set_initial_state()
 
-        self.new_btn   .set_visible(False)
-        self.save_btn  .set_visible(True)
-        self.remove_btn.set_visible(False)
-        self.quit_btn  .set_visible(False)
-        self.cancel_btn.set_visible(True)
-        
-        sel = self.users_list.get_selection().unselect_all()
-
-    def remove_click(self, widget):
-        model, row = self.users_list.get_selection().get_selected()
+    def on_remove_clicked(self, widget, data=None):
+        model, row = self.builder.get_object('users_list').get_selection().get_selected()
         if row is not None:
             self.config.delete_user(model.get_value(row, 0))
             model.remove(row)
-        self.cancel_click(None)
+        self.set_initial_state()
+        
+    def on_quit_clicked(self, widget, data=None):
+        self.exit()
 
-    def cancel_click(self, widget):
-        self.username_entry.set_text('')
-        self.password_entry.set_text('')
-        self.uri_entry     .set_text('')
-
-        self.username_entry.set_editable(False)
-        self.password_entry.set_editable(False)
-        self.uri_entry     .set_editable(False)
-
-        self.new_btn   .set_visible(True)
-        self.save_btn  .set_visible(False)
-        self.remove_btn.set_visible(False)
-        self.quit_btn  .set_visible(True)
-        self.cancel_btn.set_visible(False)
-
-    def save_click(self, widget):
-        if not self.username_entry.get_text() or \
-           not self.password_entry.get_text() or \
-           not self.uri_entry.get_text():
-            self.mf_window.show()
-        else:
-            model, row = self.users_list.get_selection().get_selected()
-            if row is not None:
-                model.set_value(row, 0, self.username_entry.get_text())
-                model.set_value(row, 1, self.password_entry.get_text())
-                model.set_value(row, 2, self.uri_entry.get_text()     )
-            else:
-                model.append(None, [self.username_entry.get_text(), \
-                                    self.password_entry.get_text(), \
-                                    self.uri_entry.get_text()])
-
-            self.config.add_user(self.username_entry.get_text(), \
-                                 self.password_entry.get_text(), \
-                                 self.uri_entry.get_text())
-            self.cancel_click(None)
-
+    def on_cancel_clicked(self, widget, data=None):
+        self.set_initial_state()
 
 if __name__ == "__main__":
     APP_NAME = 'multi-gmail-notifier'
     APP_PASS = 'multi-gmail-notifier-pass'
  
-    settings = Settings()
+    settings = Settings(APP_NAME, APP_PASS)
     settings.show()
