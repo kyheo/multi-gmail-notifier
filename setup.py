1947279fbfd00adc1a6e0ec61367c6a4fd0d06cd
Martin Marrese <marrese@gmail.com>
2011-01-27 00:52:27 -0300

    Setup update.

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "multi-gmail-notifier",
+    name = "multigmailnotifier",
     version = "0.2",
     packages = find_packages(),
     entry_points = {
         'console_scripts': [
-            'multi-gmail-notifier = multi-gmail-notifier.run:main',
+            'multigmailnotifier = multigmailnotifier.run:main',
             ], 
         },    
-    test_suite = "multi-gmail-notifier.test",
     include_package_data = True,
     author = "Martin Marrese",
     author_email = "marrese@gmail.com",
@@ -17,7 +16,7 @@ setup(
     #long_description=read('README'),
     install_requires = ['python-notify',
                         'python-indicate',
-                        'python-gobject',
-                        'python-feedparser',
+                        #'pygobject',
+                        #'feedparser',
                         ]
 )
